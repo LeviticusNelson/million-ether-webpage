4605712d65c9764cd41b94f459d187639da259da
Levi Nelson <levibb11@gmail.com>
2022-02-26 09:09:16 -0800

    better import

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,6 +1,7 @@
-import * as wasm from "rust-wasm";
+
 export default async function handler(req, res) {
 	try {
+		const wasm = await import("../../rust-wasm/pkg");
 		const url = process.env.NEXT_PUBLIC_SUPABASE_URL + "/rest/v1";
 		const image = await wasm.get_image_from_db(
 			url,
