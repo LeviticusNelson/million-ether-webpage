27ca89b6c8a32ddf4ad9a161d4e8e1bdabfbc99f
Levi Nelson <levibb11@gmail.com>
2022-02-25 20:54:27 -0800

    changed import method

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,10 +1,11 @@
 import { supabase } from "../../utils/supabaseClient";
+import * as wasm from "rust-wasm";
 
 export default async function handler(req, res) {
 	try {
 		const { get_image_from_db: getImage } = await import("rust-wasm");
 		const url = process.env.NEXT_PUBLIC_SUPABASE_URL + "/rest/v1";
-		const image = await getImage(
+		const image = await wasm.get_image_from_db(
 			url,
 			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 		);
