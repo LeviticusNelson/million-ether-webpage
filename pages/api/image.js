605fa75c9cd9ebdf07b1e419b8f22db72cc22a96
Levi Nelson <levibb11@gmail.com>
2022-02-25 21:46:28 -0800

    test to see if changing the import settings work

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,9 +1,6 @@
-import { supabase } from "../../utils/supabaseClient";
 import * as wasm from "rust-wasm";
-
 export default async function handler(req, res) {
 	try {
-		const { get_image_from_db: getImage } = await import("rust-wasm");
 		const url = process.env.NEXT_PUBLIC_SUPABASE_URL + "/rest/v1";
 		const image = await wasm.get_image_from_db(
 			url,
