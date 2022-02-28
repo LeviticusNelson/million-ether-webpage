01867f62f15390c4264c084a13b3ec8ef1411401
Levi Nelson <levibb11@gmail.com>
2022-02-28 11:20:42 -0800

    attempt with next@canary

--- next.config.js
+++ next.config.js
@@ -1,37 +1,44 @@
 // next.config.js
 
 module.exports = {
-  swcMinify: true,
-  webpack(config, { isServer, dev }) {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-    };
+	swcMinify: true,
+	webpack(config, { isServer, dev }) {
+		config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
+		config.experiments = {
+			asyncWebAssembly: true,
+			layers: true,
+		};
 
-    if (!dev && isServer) {
-      config.output.webassemblyModuleFilename = "chunks/[id].wasm";
-      config.plugins.push(new WasmChunksFixPlugin());
-    }
+		config.module.rules.push({
+			test: /\.svg$/,
+			issuer: /\.(js|ts)x?$/,
+			use: ["@svgr/webpack"],
+		});
 
-    return config;
-  },
+		if (!dev && isServer) {
+			config.output.webassemblyModuleFilename = "chunks/[id].wasm";
+			config.plugins.push(new WasmChunksFixPlugin());
+		}
+
+		return config;
+	},
 };
 
 class WasmChunksFixPlugin {
-  apply(compiler) {
-    compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
-      compilation.hooks.processAssets.tap(
-        { name: "WasmChunksFixPlugin" },
-        (assets) =>
-          Object.entries(assets).forEach(([pathname, source]) => {
-            if (!pathname.match(/\.wasm$/)) return;
-            compilation.deleteAsset(pathname);
+	apply(compiler) {
+		compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
+			compilation.hooks.processAssets.tap(
+				{ name: "WasmChunksFixPlugin" },
+				(assets) =>
+					Object.entries(assets).forEach(([pathname, source]) => {
+						if (!pathname.match(/\.wasm$/)) return;
+						compilation.deleteAsset(pathname);
 
-            const name = pathname.split("/")[1];
-            const info = compilation.assetsInfo.get(pathname);
-            compilation.emitAsset(name, source, info);
-          })
-      );
-    });
-  }
+						const name = pathname.split("/")[1];
+						const info = compilation.assetsInfo.get(pathname);
+						compilation.emitAsset(name, source, info);
+					})
+			);
+		});
+	}
 }
