aee48ea739ecc447e377999e364e526d4119a0e9
Levi Nelson <levibb11@gmail.com>
2022-03-02 08:51:39 -0800

    attempt of fixing api issue on vercel

--- next.config.js
+++ next.config.js
@@ -1,44 +1,40 @@
 // next.config.js
 
+const {join} = require('path');
+
 module.exports = {
-	swcMinify: true,
 	webpack(config, { isServer, dev }) {
-		config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
-		config.experiments = {
-			asyncWebAssembly: true,
-			layers: true,
-		};
+		config.experiments.asyncWebAssembly = true;
+		config.plugins.push(
+			new (class {
+				apply(compiler) {
+					compiler.hooks.afterEmit.tapPromise(
+						"SymlinkWebpackPlugin",
+						async (compiler) => {
+							if (isServer) {
+								const from = join(compiler.options.output.path, "../static");
+								const to = join(compiler.options.output.path, "static");
 
-		config.module.rules.push({
-			test: /\.svg$/,
-			issuer: /\.(js|ts)x?$/,
-			use: ["@svgr/webpack"],
-		});
+								try {
+									await from;
+									console.log(`${from} already exists`);
+									return;
+								} catch (error) {
+									if (error.code === "ENOENT") {
+										// No link exists
+									} else {
+										throw error;
+									}
+								}
 
-		if (!dev && isServer) {
-			config.output.webassemblyModuleFilename = "chunks/[id].wasm";
-			config.plugins.push(new WasmChunksFixPlugin());
-		}
-
+								await symlink(to, from, "junction");
+								console.log(`created symlink ${from} -> ${to}`);
+							}
+						}
+					);
+				}
+			})()
+		);
 		return config;
 	},
 };
-
-class WasmChunksFixPlugin {
-	apply(compiler) {
-		compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
-			compilation.hooks.processAssets.tap(
-				{ name: "WasmChunksFixPlugin" },
-				(assets) =>
-					Object.entries(assets).forEach(([pathname, source]) => {
-						if (!pathname.match(/\.wasm$/)) return;
-						compilation.deleteAsset(pathname);
-
-						const name = pathname.split("/")[1];
-						const info = compilation.assetsInfo.get(pathname);
-						compilation.emitAsset(name, source, info);
-					})
-			);
-		});
-	}
-}
