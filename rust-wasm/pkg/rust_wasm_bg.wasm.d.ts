3c107d78f9a1441b62e3a6a7416c271faa6294bf
Levi Nelson <levibb11@gmail.com>
2022-03-15 17:14:05 -0700

    ^ pt 2

---
+++ rust-wasm/pkg/rust_wasm_bg.wasm.d.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_pixel_free(a: number): void;
+export function pixel_encode(a: number, b: number): void;
+export function pixel_decode(a: number): number;
+export function pixel_id(a: number, b: number): void;
+export function pixel_r(a: number): number;
+export function pixel_g(a: number): number;
+export function pixel_b(a: number): number;
+export function __wbg_image_free(a: number): void;
+export function image_new(a: number, b: number, c: number, d: number): number;
+export function image_decode(a: number): number;
+export function image_width(a: number): number;
+export function image_height(a: number): number;
+export function image_get_index(a: number, b: number, c: number): number;
+export function image_get_pixel(a: number, b: number, c: number): number;
+export function image_sort_pixels(a: number): void;
+export function image_pixels(a: number, b: number): void;
+export function image_paint(a: number, b: number, c: number, d: number, e: number): void;
+export function image_encode(a: number, b: number): void;
+export function get_image_from_db(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he7f10970df57129b(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__hd76261d73af606d5(a: number, b: number, c: number, d: number): void;
