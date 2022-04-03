7b3f49c67dd479c9110c70bd5bb448d183ad2280
Levi Nelson <levibb11@gmail.com>
2022-04-03 15:17:10 -0700

    change of webpack config

--- next.config.js
+++ next.config.js
@@ -1,36 +1,18 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	webpack(config, { isServer }) {
+		if (isServer) {
+			config.output.webassemblyModuleFilename =
+				"./../static/wasm/[modulehash].wasm";
+		} else {
+			config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
+		}
+		config.experiments = { asyncWebAssembly: true, layers: true };
+		config.optimization.moduleIds = "named";
 
-module.exports = {
-	webpack(config, { isServer, dev }) {
-	  config.experiments = {
-		asyncWebAssembly: true,
-		layers: true,
-	  };
-  
-	  if (!dev && isServer) {
-		config.output.webassemblyModuleFilename = "chunks/[id].wasm";
-		config.plugins.push(new WasmChunksFixPlugin());
-	  }
-  
-	  return config;
+		return config;
 	},
-  };
-  
-  class WasmChunksFixPlugin {
-	apply(compiler) {
-	  compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
-		compilation.hooks.processAssets.tap(
-		  { name: "WasmChunksFixPlugin" },
-		  (assets) =>
-			Object.entries(assets).forEach(([pathname, source]) => {
-			  if (!pathname.match(/\.wasm$/)) return;
-			  compilation.deleteAsset(pathname);
-  
-			  const name = pathname.split("/")[1];
-			  const info = compilation.assetsInfo.get(pathname);
-			  compilation.emitAsset(name, source, info);
-			})
-		);
-	  });
-	}
-  }
+};
+
+module.exports = nextConfig;
